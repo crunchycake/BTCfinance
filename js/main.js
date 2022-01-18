b97f88b04a010ff67e4de0519bffd8c3cc852906
Mateusz Cieslik <mateuszcieslik@gmail.com>
2022-01-18 18:49:33 +0100

    added js to bar

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const navMobile = document.querySelector('.nav-mobile');
+const navBtn = document.querySelector('.hamburger');
+const footerYear = document.querySelector('.footer__year');
+
+const handleCurrentYear = () => {
+    const year = (new Date).getFullYear();
+    footerYear.innerText = year;
+}
+handleCurrentYear();
+
+
+
+const handleNav = () => {
+    navBtn.classList.toggle('is-active');
+    navMobile.classList.toggle('nav-mobile--active');
+}
+navBtn.addEventListener('click', handleNav);
